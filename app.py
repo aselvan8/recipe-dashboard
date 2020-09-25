f92e09cd1133db4d7fccb4406bc18ef31d6378c6
Tameka Kuar <tamekakuar17@gmail.com>
2020-09-24 20:11:09 -0400

    add semi working js and py files

--- app.py
+++ app.py
@@ -1,79 +1,61 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import pymongo
 import json
 from bson.json_util import dumps
 from bson.objectid import ObjectId
-
+​
 app = Flask(__name__)
-
+​
 # setup mongo connection
 conn = "mongodb://localhost:27017"
 client = pymongo.MongoClient(conn)
-
+​
 # connect to mongo db and collection
 db = client.recipeDB
 recipes = db.recipe
+​
+​
+​
+# home pages
 
 
-
-#home pages
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
+​
 
-#API Call route
-@app.route("/api_call/<recipe_id>")
-def apipull(api_call):
-    data = list(recipes.find())
-    recipe_json = dumps(data)
-
-    # render an index.html template and pass it the data you retrieved from the database
-    return render_template("index.html", recipe = data)
+​
+# route to load the last page based on the recipe id extracted from clicking an image
 
 
-
-#route to load the last page based on the recipe id extracted from clicking an image
 @app.route("/final_recipe/<recipe_id>")
 def load_recipe(recipe_id):
-    #type img url to be the recipe_id
-    #return render_template("index2.html")
-    data = recipes.find_one({"_id" : ObjectId(recipe_id)})
+    # type img url to be the recipe_id
+    # #return render_template("index2.html")
+    data = recipes.find_one({"_id": ObjectId(recipe_id)})
     print(data)
-
+    print(recipe_id)
     # render an index.html template and pass it the data you retrieved from the database
-    return render_template("recipe.html", recipe = data)
+    return render_template("recipe.html", recipe=data)
 
 
-# @app.route("/recipe")
-# def test():
-#     #recipe_id = "5f661b49039ded9690d3fb6c"
+​
 
-#     #data = recipes.find_one()
-    
 
-#     data = recipes.find_one({"_id" : ObjectId('5f661b49039ded9690d3fb6c')})
-#     #data = recipes.find({"recipe.label" :{$regex: ".*Chicken.*"}})
-#     print(type(data))
-
-#     recipe_json = dumps(data)
-
-#     # render an index.html template and pass it the data you retrieved from the database
-#     return recipe_json
-
+@app.route("/recipe", methods=['GET', 'POST'])
+def test():
+    if request.method == 'POST':
+        print(request.json)
+        return jsonify("got it")
+    if request.method == 'GET':
+        return "you made a get request"
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-    
-    # for dict_value in recipe.recipe.totalDaily:
-    #     for k, v in dict_value.items():
-    #         dict_value[k] = round(v,2)
-    
-    # recipe = {k:round(v) for k, v in recipe[0].recipe.totalNutrients.items()}
-    
+​
+​
+​
+​
--- static/js/search.js
+++ static/js/search.js
@@ -1,35 +1,61 @@
 var params;
-d3.select(".form-group").on("change", function(){
-    
+d3.select(".form-group").on("change", function () {
+​
     d3.event.preventDefault();
-    
+​
     var diet = d3.select("#diet").property("value");
     console.log(diet);
-
+​
     healthArray = [];
     healthCheck = d3.select(".restriction").selectAll(".health");
-    
-    healthCheck.each(function(){
+​
+    healthCheck.each(function () {
         var thisBox = d3.select(this);
         if (thisBox.property("checked")) {
             healthArray.push(thisBox.property("value"));
         }
     });
     console.log(healthArray)
-    
+​
     var calorieMax = d3.select(".slider").property("value");
     console.log(calorieMax);
-    
+​
     var query = d3.select("#query").property("value");
     console.log(query);
-    
+​
     params = {
         "diet": diet,
         "health": healthArray,
         "calories": calorieMax,
         "query": query
     };
-
-    
+​
+​
 });
 console.log(params);
+var button = d3.select('#filter-btn')
+button.on("click", function () {
+    // d3.json(`/api_call/${search_params}`, function (data) {
+    //     console.log(search_params);
+    //     console.log(data);
+    // })
+​
+    var search_params = {
+        "query": "chicken",
+        "diet": "low-carb",
+        "health": ["peanut-free", "vegetarian"],
+        "calories": "100-300"
+    };
+​
+    fetch('/recipe', {
+        method: 'POST',
+        body: JSON.stringify(search_params),
+        headers: new Headers({
+            "content-type": "application/json"
+          })
+      }).then(function (res) {
+        return res.json()
+      }).then((data) => {
+        // do somthing with data
+      })
+});
